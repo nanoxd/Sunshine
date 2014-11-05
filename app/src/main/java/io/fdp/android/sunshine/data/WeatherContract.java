d729e38f83e16895b6ec22f2366e5f027dbe5e48
Fernando Paredes <nano@fdp.io>
2014-11-04 21:37:14 -0800

    Add LocationEntry for database

--- app/src/main/java/io/fdp/android/sunshine/data/WeatherContract.java
+++ app/src/main/java/io/fdp/android/sunshine/data/WeatherContract.java
@@ -38,4 +38,17 @@ public class WeatherContract {
         // Degrees are meteorological degrees (e.g, 0 is north, 180 is south).  Stored as floats.
         public static final String COLUMN_DEGREES = "degrees";
     }
+
+    /* Inner class that defines the table contents of the location table */
+    public static final class LocationEntry implements BaseColumns {
+
+        public static final String TABLE_NAME = "location";
+
+        // Column with the the location setting
+        public static final String COLUMN_LOCATION_SETTING = "location_setting";
+        public static final String COLUMN_CITY_NAME = "city_name";
+        public static final String COLUMN_COORD_LAT = "lat";
+        public static final String COLUMN_COORD_LONG = "long";
+
+    }
 }
