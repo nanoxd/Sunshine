a0bab68ab79ee5060f31f7a3f3f8b5c0f3f2550e
Fernando Paredes <nano@fdp.io>
2014-11-05 11:39:10 -0800

    Add weather provider

--- app/src/main/java/io/fdp/android/sunshine/data/WeatherProvider.java
+++ app/src/main/java/io/fdp/android/sunshine/data/WeatherProvider.java
@@ -18,10 +18,12 @@ public class WeatherProvider extends ContentProvider {
     private static final int LOCATION_ID = 301;
 
     private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private WeatherDbHelper mOpenHelper;
 
     @Override
     public boolean onCreate() {
-        return false;
+        mOpenHelper = new WeatherDbHelper(getContext());
+        return true;
     }
 
     @Override
@@ -31,7 +33,21 @@ public class WeatherProvider extends ContentProvider {
 
     @Override
     public String getType(Uri uri) {
-        return null;
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case WEATHER_WITH_LOCATION_AND_DATE:
+                return WeatherContract.WeatherEntry.CONTENT_ITEM_TYPE;
+            case WEATHER_WITH_LOCATION:
+                return WeatherContract.WeatherEntry.CONTENT_TYPE;
+            case WEATHER:
+                return WeatherContract.WeatherEntry.CONTENT_TYPE;
+            case LOCATION:
+                return WeatherContract.LocationEntry.CONTENT_TYPE;
+            case LOCATION_ID:
+                return WeatherContract.LocationEntry.CONTENT_ITEM_TYPE;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
     }
 
     @Override
